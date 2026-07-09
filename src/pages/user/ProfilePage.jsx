5d5ca70e5b249c25e4d0d93b16a76c86d6acd812
kanokpol kulsri <kanokpolkulsri@gmail.com>
2026-07-08 23:17:48 -0400

    deploy: 2026-07-08 23:17:48

--- src/pages/user/ProfilePage.jsx
+++ src/pages/user/ProfilePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useUser, useUserDispatch } from '../../data/userStore';
 import { auth, signOut } from '../../data/firebase';
